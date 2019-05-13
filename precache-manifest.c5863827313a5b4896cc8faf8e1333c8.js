28e5ba97e7f0b377ff4b12d61bf54e4496c4d855
Bikky Bhujel <bicky.bhujel@gmail.com>
2019-05-13 10:53:09 +0545

    Updates

---
+++ precache-manifest.c5863827313a5b4896cc8faf8e1333c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393674e2583db05b81e3ff8fd42e209e",
+    "url": "/league_venue/index.html"
+  },
+  {
+    "revision": "cde5b22e16437277096e",
+    "url": "/league_venue/static/css/main.5014ded5.chunk.css"
+  },
+  {
+    "revision": "d6ab76efcba51b0a0944",
+    "url": "/league_venue/static/js/2.473c2310.chunk.js"
+  },
+  {
+    "revision": "cde5b22e16437277096e",
+    "url": "/league_venue/static/js/main.7037a7cf.chunk.js"
+  },
+  {
+    "revision": "1c64db8ce43d422cc300",
+    "url": "/league_venue/static/js/runtime~main.ce130b6c.js"
+  },
+  {
+    "revision": "996dd455d77dfd11c0dc6718ada67bc4",
+    "url": "/league_venue/static/media/slide_one.996dd455.jpg"
+  },
+  {
+    "revision": "0588c4c5eaca4cc682d52744895cad8b",
+    "url": "/league_venue/static/media/slide_three.0588c4c5.jpg"
+  },
+  {
+    "revision": "828dff574cc76c4b15fc33ca47dc0af6",
+    "url": "/league_venue/static/media/slide_two.828dff57.jpg"
+  }
+]);
